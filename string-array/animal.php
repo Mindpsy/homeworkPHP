baff1aeaf463f3b18899cbdfcd10aa6366b774f2
Kalshnikov.Viktor <kalashnikov.marketing@mail.ru>
2018-08-16 13:01:45 +0300

    qwerewq

--- string-array/animal.php
+++ string-array/animal.php
@@ -1,3 +1,46 @@
 <?php
+$animalsArray = array("Africa" => ["Potamochoerus porcus", 
+                                    "Cephalophinae", 
+                                    "Hexaprotodon liberiensis"], 
+                      "Southern America" => ["Microsciurus flaviventer",
+                                    "Inia geoffrensis",
+                                    "Cebus albifrons"], 
+                      "Australia" => ["Thylacinus cynocephalus",
+                                      "Trichosurus vulpecula",
+                                      "Zaglossus bruijni"],
+                      "Eurasia" => ["Rangifer tarandus",
+                                    "Vulpes",
+                                    "Vulpes lagopus"], 
+                      "North America" => ["Bubo scandiacus",
+                                          "Cricetinae",
+                                          "Ursus arctos horribilis"]);
+/*
+Континенты и животные:
 
+Африка
+Potamochoerus porcus
+Cephalophinae
+Hexaprotodon liberiensis
+
+Южная Америка
+Microsciurus flaviventer
+Inia geoffrensis
+Cebus albifrons
+
+Австралия
+Thylacinus cynocephalus
+Trichosurus vulpecula
+Zaglossus bruijni
+
+Евразия
+Rangifer tarandus
+Vulpes
+Vulpes lagopus
+
+Северная Америка
+Bubo scandiacus
+Cricetinae
+Ursus arctos horribilis
+
+*/
 ?>
