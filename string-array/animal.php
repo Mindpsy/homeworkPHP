025a0372681b06d99fbb85de91ba4b0bf8d67cfb
Kalshnikov.Viktor <kalashnikov.marketing@mail.ru>
2018-08-16 15:37:11 +0300

    Добавил циклы и функции, теперь получаем на выходе массив с животными из двух слов.Остается придумать алгоритм для перемешивания слов уникальным образом.

--- string-array/animal.php
+++ string-array/animal.php
@@ -1,46 +1,35 @@
 <?php
-$animalsArray = array("Africa" => ["Potamochoerus porcus", 
-                                    "Cephalophinae", 
-                                    "Hexaprotodon liberiensis"], 
-                      "Southern America" => ["Microsciurus flaviventer",
-                                    "Inia geoffrensis",
-                                    "Cebus albifrons"], 
-                      "Australia" => ["Thylacinus cynocephalus",
-                                      "Trichosurus vulpecula",
-                                      "Zaglossus bruijni"],
-                      "Eurasia" => ["Rangifer tarandus",
-                                    "Vulpes",
-                                    "Vulpes lagopus"], 
-                      "North America" => ["Bubo scandiacus",
-                                          "Cricetinae",
-                                          "Ursus arctos horribilis"]);
-/*
-Континенты и животные:
+$animalsArray = array("Africa" => ["Potamochoerus porcus", "Cephalophinae", "Hexaprotodon liberiensis"], 
+    "Southern America" => ["Microsciurus flaviventer", "Inia geoffrensis", "Cebus albifrons"], 
+    "Australia" => ["Thylacinus cynocephalus", "Trichosurus vulpecula", "Zaglossus bruijni"],
+    "Eurasia" => ["Rangifer tarandus", "Vulpes", "Vulpes lagopus"], 
+    "North America" => ["Bubo scandiacus", "Cricetinae", "Ursus arctos horribilis"]);
 
-Африка
-Potamochoerus porcus
-Cephalophinae
-Hexaprotodon liberiensis
+// i am wrote function for testing count of words
+function isMoreThanTwo ($value) {
+    $words = explode(" ", $value);
+    if(count($words) == 2) {
+        return true;
+    } else {
+        return false;
+    }
+}
 
-Южная Америка
-Microsciurus flaviventer
-Inia geoffrensis
-Cebus albifrons
+function pluckAndReturnMassive ($massive) {
+    $newArray = [];
+    foreach ($massive as $key => $value) {
+        for ($i=0; $i < count($value); $i++) { 
+            if(isMoreThanTwo($value[$i])) {
+                $newArray[] = $value[$i];
+            }
+        }
+    }
+    return $newArray;
+}
 
-Австралия
-Thylacinus cynocephalus
-Trichosurus vulpecula
-Zaglossus bruijni
+$result = pluckAndReturnMassive($animalsArray);
+foreach ($result as $key => $value) {
+    echo "$value </br>";
+}
 
-Евразия
-Rangifer tarandus
-Vulpes
-Vulpes lagopus
-
-Северная Америка
-Bubo scandiacus
-Cricetinae
-Ursus arctos horribilis
-
-*/
 ?>
