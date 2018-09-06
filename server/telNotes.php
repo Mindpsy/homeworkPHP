3b874eadb11ed192c1c7db35340c76758bada2cb
Kalshnikov.Viktor <kalashnikov.marketing@mail.ru>
2018-09-06 21:35:38 +0300

    -

--- server/telNotes.php
+++ server/telNotes.php
@@ -1,32 +1,31 @@
-<?php
-echo "<!DOCTYPE>";
-echo "<html>";
-echo "<head>";
-echo "<title>";
-echo "</title>";
-echo "<meta charset='UTF-8'>";
-echo "<meta name='vievport' content='width=device-width'>";
-echo "</head>";
-echo "<body>";
-echo "<table>";
-echo "<tr>";
-echo "<td>Имя</td>";
-echo "<td>Фамилия</td>";
-echo "<td>Адрес</td>";
-echo "<td>Номер телефона</td>";
-echo "</tr>";
-                
-$jsonNotes = file_get_contents("./jsonNotes.json");
-$objectNotes = json_decode($jsonNotes);
-foreach($objectNotes as $value ) {
-    echo "<tr>";
-    echo "<td>$value->firstName</td>";
-    echo "<td>$value->lastName</td>";
-    echo "<td>$value->address</td>";
-    echo "<td>$value->phoneNumber</td>";
-    echo "</tr>";
-}
-echo "</table>";
-echo "</body>";
-echo "</html>";
-    ?>
+<!DOCTYPE>
+    <html>
+        <head>
+            <title></title>
+            <meta charset='UTF-8'>
+            <meta name='vievport' content='width=device-width'>
+        </head>
+        <body>
+            <table>
+                <tr>
+                    <td>Имя</td>
+                    <td>Фамилия</td>
+                    <td>Адрес</td>
+                    <td>Номер телефона</td>
+                </tr>
+                <?php               
+                    $jsonNotes = file_get_contents("./jsonNotes.json");
+                    $objectNotes = json_decode($jsonNotes, true);
+                    foreach($objectNotes as $value ) {
+                        echo "<tr>";
+                        echo "<td>$value[firstName]</td>";
+                        echo "<td>$value[lastName]</td>";
+                        echo "<td>$value[address]</td>";
+                        echo "<td>$value[phoneNumber]</td>";
+                        echo "</tr>";
+                    }
+                    ?>
+            </table>
+        </body>
+    </html>
+    
